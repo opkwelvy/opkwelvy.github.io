a81be190c5f2b3d7c07994608ad2029ed7f00304
Kelvy Corrêa <79207622+opkwelvy@users.noreply.github.com>
2024-04-21 17:59:15 -0300

    finished functions of change projects

--- dist/script.js
+++ dist/script.js
@@ -6,38 +6,33 @@ button.addEventListener('click', () => {
 
 window.addEventListener('scroll', () => {
   window.scrollY > 100
-    ? button.classList.add('active')
-    : button.classList.remove('active');
+    ? button.classList.add('visible')
+    : button.classList.remove('visible');
 });
 
 class Slide {
   container;
   slides;
-  controls;
+
   time;
   index;
   slide;
   paused;
   timeout;
   pausedTimeout;
-  thumbItems;
-  thumb;
-  constructor(container, slides, controls, time = 5000) {
+  constructor(container, slides, time = 5000) {
     this.container = container;
     this.slides = slides;
-    this.controls = controls;
     this.time = time;
     this.index = 0;
     this.slide = this.slides[this.index];
     this.paused = false;
     this.timeout = null;
-    this.pausedTimeout = null;
-    this.thumbItems = null;
-    this.thumb = null;
     this.init();
   }
   hide(el) {
     el.classList.remove('active');
+    el.classList.add('disabled');
     if (el instanceof HTMLVideoElement) {
       el.currentTime = 0;
       el.pause();
@@ -47,12 +42,8 @@ class Slide {
     this.index = index;
     this.slide = this.slides[this.index];
     this.slides.forEach((el) => this.hide(el));
-    if (this.thumbItems) {
-      this.thumb = this.thumbItems[this.index];
-      this.thumbItems.forEach((el) => el.classList.remove('active'));
-      this.thumb.classList.add('active');
-    }
     this.slide.classList.add('active');
+    this.slide.classList.remove('disabled');
     this.auto(this.time);
     if (this.slide instanceof HTMLVideoElement) {
       this.autoVideo(this.slide);
@@ -72,7 +63,8 @@ class Slide {
   auto(time) {
     this.timeout?.clear();
     this.timeout = new Timeout(() => this.next(), time);
-    if (this.thumb) this.thumb.style.animationDuration = `${time}ms`;
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.slides[next].classList.remove('disabled');
   }
   prev() {
     if (this.paused) return;
@@ -82,12 +74,13 @@ class Slide {
   next() {
     if (this.paused) return;
     const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.slides[next].classList.remove('disabled');
     this.show(next);
   }
   pause() {
     document.body.classList.add('paused');
     this.pausedTimeout = new Timeout(() => {
-      this.timeout?.pause();
+      this.timeout.pause();
       this.paused = true;
       this.thumb?.classList.add('paused');
       if (this.slide instanceof HTMLVideoElement) this.slide.pause();
@@ -104,30 +97,33 @@ class Slide {
     }
   }
   addControl() {
-    const prevButtuon = document.createElement('button');
-    const nextButtuon = document.createElement('button');
-    prevButtuon.innerText = 'Slide anterior';
-    nextButtuon.innerText = 'Próximo slide';
-    this.controls.appendChild(prevButtuon);
-    this.controls.appendChild(nextButtuon);
-    this.controls.addEventListener('pointerdown', () => this.pause());
-    document.addEventListener('pointerup', () => this.continue());
-    document.addEventListener('touchend', () => this.continue());
-    nextButtuon.addEventListener('pointerup', () => this.next());
-    prevButtuon.addEventListener('pointerup', () => this.prev());
-  }
-  addThumbItems() {
-    const thumbContainer = document.createElement('div');
-    thumbContainer.id = 'slide-thumb';
-    for (let i = 0; i < this.slides.length; i++) {
-      thumbContainer.innerHTML += `<span><span class="thumb-item"></span></span>`;
-    }
-    this.controls.appendChild(thumbContainer);
-    this.thumbItems = Array.from(document.querySelectorAll('.thumb-item'));
+    const backButton = document.querySelectorAll('.back');
+    const pauseButton = document.querySelectorAll('.pause');
+    const playButton = document.querySelectorAll('.play');
+    const nextButton = document.querySelectorAll('.next');
+    pauseButton.forEach((button) =>
+      button.addEventListener('pointerup', () => {
+        this.pause();
+        playButton.forEach((button) => button.classList.remove('played'));
+        pauseButton.forEach((button) => button.classList.add('paused'));
+      }),
+    );
+    playButton.forEach((button) =>
+      button.addEventListener('pointerup', () => {
+        this.continue();
+        playButton.forEach((button) => button.classList.add('played'));
+        pauseButton.forEach((button) => button.classList.remove('paused'));
+      }),
+    );
+    nextButton.forEach((button) =>
+      button.addEventListener('pointerup', () => this.next()),
+    );
+    backButton.forEach((button) =>
+      button.addEventListener('pointerup', () => this.prev()),
+    );
   }
   init() {
     this.show(this.index);
-    this.addThumbItems();
     this.addControl();
   }
 }
@@ -158,29 +154,51 @@ class Timeout {
   }
 }
 
-const container = document.querySelector('.projetos-lista');
-const elements = document.querySelector('.projetos-lista').children;
-const controls = document.getElementById('slide-control');
+const container = document.querySelectorAll('.projetos-lista');
+const elements = document.querySelectorAll('.projetos-lista');
+const containerDev = container[0];
+const containerDesign = container[1];
+const elementsDev = elements[0];
+const elementsDesign = elements[1];
 
-if (container && elements && controls && elements.length) {
-  new Slide(container, Array.from(elements), controls, 3000);
+const elementsDevArray = Array.from(elementsDev.children);
+elementsDevArray.pop();
+
+if (containerDev && elementsDevArray && elementsDevArray.length) {
+  new Slide(containerDev, elementsDevArray, 5000);
 }
+const elementsDesignArray = Array.from(elementsDesign.children);
 
+if (containerDesign && elementsDesignArray && elementsDesignArray.length) {
+  new Slide(containerDesign, elementsDesignArray, 5000);
+}
 const devButton = document.querySelector('.active-dev');
 const designButton = document.querySelector('.active-design');
-const devTitle = document.querySelector('.desenvolvimento');
-const designTitle = document.querySelector('.design');
+const devTitle = document.querySelector('.sub .dev');
+const designTitle = document.querySelector('.sub .design');
 const projectContainer = document.querySelector('.projetos');
+const projectsContentDev = document.querySelector('.projetos-lista.dev');
+const projectsContentDesign = document.querySelector('.projetos-lista.design');
 
 devButton.addEventListener('click', () => {
   projectContainer.classList.add('dev');
+  projectsContentDev.classList.add('active');
   devTitle.classList.add('active');
+  devButton.classList.add('active');
+
   projectContainer.classList.remove('design');
   designTitle.classList.remove('active');
+  designButton.classList.remove('active');
+  projectsContentDesign.classList.remove('active');
 });
 designButton.addEventListener('click', () => {
   projectContainer.classList.add('design');
+  projectsContentDesign.classList.add('active');
   designTitle.classList.add('active');
+  designButton.classList.add('active');
+
   projectContainer.classList.remove('dev');
   devTitle.classList.remove('active');
+  devButton.classList.remove('active');
+  projectsContentDev.classList.remove('active');
 });
